7eb595713165a31399a88e47b11b1d3218e1dc87
Madeleine Filloux <mfilloux@gmail.com>
2018-04-14 15:38:33 -0500

    movie search and random do what it says functionality

--- liri.js
+++ liri.js
@@ -3,14 +3,27 @@ require("dotenv").config();
 const Spotify = require("node-spotify-api");
 const Twitter = require("twitter");
 const request = require("request");
+const fs = require("fs");
 const keys = require("./keys.js");
 
 // GLOBAL VARIABLES
-const command = process.argv[2];
+let command = process.argv[2];
 let parameter = process.argv[3];
 
 // COMMANDS
 // * `movie-this`
+// * Title of the movie.
+// * Year the movie came out.
+// * IMDB Rating of the movie.
+// * Rotten Tomatoes Rating of the movie.
+// * Country where the movie was produced.
+// * Language of the movie.
+// * Plot of the movie.
+// * Actors in the movie.
+// default:
+// If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
+// It's on Netflix!
+
 
 // * `do-what-it-says`
 
@@ -27,7 +40,7 @@ function myTweets() {
             // console.log(tweets);
             tweets.forEach((tweet, i) => {
                 console.log("----------------------------------------------");
-                console.log(`Tweet ${i+1}`);
+                console.log(`Tweet ${i + 1}`);
                 console.log(`"${tweet.text}"`);
                 console.log(`Created at: ${tweet.created_at}`); // TODO: format time better. this is UTC time.
             });
@@ -74,11 +87,57 @@ function spotifySong() {
 }
 
 function movieThis() {
+    if (parameter) {
+        const queryURL = `http://www.omdbapi.com/?apikey=trilogy&t=${parameter}`;
+        request(queryURL, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                const movieInfo = JSON.parse(body);
+                const rottenTomatoesObj = movieInfo.Ratings.find(rating => rating.Source === "Rotten Tomatoes");
+                console.log("----------------------------------------------");
+                console.log(`Title: ${movieInfo.Title}`);
+                console.log(`Year: ${movieInfo.Year}`);
+                console.log(`IMDB Rating: ${movieInfo.imdbRating}`);
+                console.log(`Rotten Tomatoes Rating: ${rottenTomatoesObj.Value}`);
+                console.log(`Country: ${movieInfo.Country}`);
+                console.log(`Language: ${movieInfo.Language}`);
+                console.log(`Plot: ${movieInfo.Plot}`);
+                console.log(`Actors: ${movieInfo.Actors}`);
+                console.log("----------------------------------------------");
+            } else {
+                console.log(`Error occurred: ${error}`);
+            }
+        });
+    } else {
+        console.log("----------------------------------------------");
+        console.log(`If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/`);
+        console.log("It's on Netflix!");
+        console.log("----------------------------------------------");
+    }
+}
 
+function processCommand() {
+    switch (command) {
+    case "my-tweets":
+        myTweets();
+        break;
+    case "spotify-this-song":
+        spotifySong();
+        break;
+    case "movie-this":
+        movieThis();
+        break;
+    default:
+        console.log("Your argument is not valid. Try [my-tweets|spotify-this-song|movie-this|do-what-it-says].");
+    }
 }
 
 function doWhatItSays() {
-
+    fs.readFile("random.txt", "utf8", (error, data) => {
+        const randArgs = data.split(",");
+        command = randArgs[0];
+        parameter = randArgs[1];
+        processCommand();
+    });
 }
 
 // RUN THE APP
@@ -86,19 +145,8 @@ if (parameter) {
     parameter = parameter.split(" ").join("+");
 }
 
-switch (command) {
-case "my-tweets":
-    myTweets();
-    break;
-case "spotify-this-song":
-    spotifySong();
-    break;
-case "movie-this":
-    console.log("searching movie info");
-    break;
-case "do-what-it-says":
-    console.log("doing what it says");
-    break;
-default:
-    console.log("Your argument is not valid. Try [my-tweets|spotify-this-son|movie-this|do-what-it-says].");
+if (command === "do-what-it-says") {
+    doWhatItSays();
+} else {
+    processCommand();
 }
